3dcc9a5f325295550402f515057622792b0b3cfd
null <uclort@gmail.com>
2022-03-04 13:42:05 +0800

    Update wifi_proxy.js

--- wifi_proxy.js
+++ wifi_proxy.js
@@ -1,12 +1,13 @@
 /*
-WiFi_Proxy = script-path=https://cdn.jsdelivr.net/gh/uclort/Rules@master/wifi_proxy.js,type=event,event-name=network-changed,control-api=true
+WiFi_Proxy = script-path=https://raw.githubusercontent.com/tangke612/GFW/master/Surge/Scripts/wifi_proxy.js,type=event,event-name=network-changed,control-api=true
 */ 
-
-var wifiname = $network.wifi.ssid;
-var proxyWifi = ["GWM"];
-if (proxyWifi.includes(wifiname)) {
-  $surge.setSelectGroupPolicy("GW", "ProxyMan")
-} else {
-  $surge.setSelectGroupPolicy("GW", "Direct")
+if ($environment.system === "iOS") {
+  var wifiname = $network.wifi.ssid;
+  var proxyWifi = ["GWM"];
+  if (proxyWifi.includes(wifiname)) {
+    $surge.setSelectGroupPolicy("GW", "ProxyMan")
+  } else {
+    $surge.setSelectGroupPolicy("GW", "Direct")
+  }
 }
 $done();
