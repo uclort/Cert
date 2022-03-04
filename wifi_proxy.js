d2624f29cca5a486558ff8e8be7230e94702bb7e
null <uclort@gmail.com>
2022-03-04 13:24:07 +0800

    Update wifi_proxy.js

--- wifi_proxy.js
+++ wifi_proxy.js
@@ -1,5 +1,5 @@
 /*
-WiFi_Proxy = script-path=https://raw.githubusercontent.com/uclort/Rules/master/wifi_proxy.js,type=event,event-name=network-changed,control-api=true
+WiFi_Proxy = script-path=https://cdn.jsdelivr.net/gh/uclort/Rules@master/wifi_proxy.js,type=event,event-name=network-changed,control-api=true
 */ 
 
 var wifiname = $network.wifi.ssid;
